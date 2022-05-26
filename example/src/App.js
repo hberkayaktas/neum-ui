2219b80fb4d68c0f8e6da72e1a5cb2648f7a811b
Hami Berkay AKTAŞ <hberkayaktas@gmail.com>
2022-05-26 03:55:16 +0300

    Many Components Added
    circular progressbar,loader component,grid component, text area component, title variations

--- example/src/App.js
+++ example/src/App.js
@@ -1,48 +1,276 @@
 import React from 'react'
 
-import { RegularCheckboxIOS,RegularCheckbox,CircleProgress,Progress,Chart,ExampleComponent,ButtonIconRounded, Button, ButtonIL,ButtonIR, Select,Radio,ButtonSq,ButtonPressed,ButtonwIcon,ButtonSmallL,ButtonSmallR,Input,Checkbox } from 'neum-ui'
+import {
+  ButtonGroup,
+  ButtonG,
+  Dropdown,
+  Textarea,
+  TextMuted,
+  Text,
+  ButtonSM,
+  ButtonLG,
+  Title,
+  BoxMorph,
+  Box,
+  Grid,
+  Loading,
+  RegularCheckboxIOS,
+  RegularCheckbox,
+  CircleProgress,
+  Progress,
+  Chart,
+  ExampleComponent,
+  ButtonIconRounded,
+  Button,
+  ButtonIL,
+  ButtonIR,
+  Select,
+  Radio,
+  ButtonSq,
+  ButtonPressed,
+  ButtonwIcon,
+  ButtonSmallL,
+  ButtonSmallR,
+  Input,
+  Checkbox
+} from 'neum-ui'
 import 'neum-ui/dist/index.css'
 
 const App = () => {
-  let deneme = true;
-  let degerler = [50,20,75,30,85,10,99,2];
-  const onChangeInput = () =>{
-    console.log(this);
+  let deneme = true
+  let degerler = [50, 20, 75, 30, 85, 10, 99, 2]
+  const onChangeInput = () => {
+    console.log(this)
   }
   return (
     <>
+      <table >
+        <tr>
+          <td>Titles</td>
+          <td>Button Size</td>
+          <td>Button variations</td>
+          <td>Checkbox variations</td>
+          <td>Square Button variations</td>
+        </tr>
+        <tr>
+          <td>
+            <Title hL={1}>Title Variations</Title>
+            <br />
+            <Title hL={2}>Title Variations</Title>
+            <br />
+            <Title hL={3}>Title Variations</Title>
+            <br />
+            <Title hL={4}>Title Variations</Title>
+            <br />
+            <Title hL={5}>Title Variations</Title>
+            <br />
+            <Title hL={6}>Title Variations</Title>
+            <br />
+          </td>
+          <td>
+            <ButtonSM href={'hamiberkayaktas.tk'}>This is Button</ButtonSM>
+            <br />
+            <Button href={'hamiberkayaktas.tk'}>This is Button</Button>
+            <br />
+            <ButtonLG href={'hamiberkayaktas.tk'}>This is Button</ButtonLG>
+            <br />
+          </td>
+          <td>
+            <Button href={'hamiberkayaktas.tk'}>This is Button</Button>
+            <ButtonPressed>Pressed Button</ButtonPressed>
+            <br />
+
+            <ButtonIL icon='fas fa-user-plus'>
+              Button with Left side icon 
+            </ButtonIL>
+            <br />
+            <ButtonIR icon='fas fa-user-plus'>
+              Button with Right side icon 
+            </ButtonIR>
+            <br />
+            <ButtonwIcon icon='fas fa-arrow-right' />
+            <ButtonwIcon icon='fas fa-arrow-left' />
+            <br />
+
+            <ButtonSmallL icon='fas fa-share-alt'> Share</ButtonSmallL>
+            <br />
+            <ButtonSmallR icon='fas fa-map-marker-alt'> Label</ButtonSmallR>
+          </td>
+          <td>
+            <RegularCheckboxIOS>Ios Checkbox</RegularCheckboxIOS>
+            <br />
+            <RegularCheckboxIOS checked={true}>Ios Checkbox</RegularCheckboxIOS>
+            <br />
+            <RegularCheckbox>Regular Checkbox</RegularCheckbox>
+            <br />
+            <RegularCheckbox checked={true}>Regular Checkbox</RegularCheckbox>
+          </td>
+          <td>
+            <ButtonSq>Aa</ButtonSq>
+            <br />
+          </td>
+        </tr>
+        <tr>
+          <td>Form Component</td>
+          <td>Chart Component</td>
+          <td>Progress Component</td>
+          <td>Text Component</td>
+          <td>Other Component</td>
+        </tr>
+        <tr>
+          <td>
+            <Input placeholder='Search For ...' />
+            <ButtonIconRounded icon='fas fa-search' />
+            <br />
+            <Select name='cars' id='cars'>
+              <option value='volvo'>Volvo</option>
+              <option value='saab'>Saab</option>
+              <option value='mercedes'>Mercedes</option>
+              <option value='audi'>Audi</option>
+            </Select>
+            <br />
+            <Radio>Radio form component</Radio><br/>
+            <Radio checked={true}>Radio form component</Radio><br/>
+            <Textarea placeholder='Textarea Component ......'></Textarea>
+          </td>
+          <td>
+            <Chart
+              degerler={[50, 20, 75, 30, 85, 10, 99, 5]}
+              w={'200px'}
+              h={'200px'}
+            />
+          </td>
+          <td>
+            <Progress value={38} w={'250px'} h={'10px'} />
+            <CircleProgress value={77} w={'200px'} h={'200px'} />
+            <CircleProgress value={32} w={'100px'} h={'100px'} />
+            <Loading w={'50px'} h={'50px'} />
+          </td>
+          <td>
+          <Title hL={2}>Title Variations</Title><br/>
+          <Text>Text Component</Text>
+          <TextMuted>Text Muted Component</TextMuted>
+          </td>
+          <td>
+            Grid Component<br/>
+  
+            <Grid col={5} rows={3} w={'100%'} key={1}>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+            </Grid>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Dropdown>{[{link:'www.twitter.com',content:'Twitter'},{link:'www.facebook.com',content:'Facebook'},{link:'www.youtube.com',content:'Youtube'}]}</Dropdown>
+            <Dropdown>{[{link:'www.twitter.com',content:'Twitter'},{link:'www.facebook.com',content:'Facebook'},{link:'www.youtube.com',content:'Youtube'}]}</Dropdown>
+          </td>
+          <td>
+            <ButtonGroup>
+              <ButtonG href={'/1'}>1</ButtonG>
+              <ButtonG href={'/2'}>2</ButtonG>
+              <ButtonG href={'/3'}>3</ButtonG>
+            </ButtonGroup>
+          </td>
+        </tr>
+      </table>
+      <Grid col={3} rows={3} key={2}>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+              <Box style={{background:'Red'}}>.</Box>
+            </Grid>
+      <Box>div denemesi</Box>
+      <BoxMorph style={{ width: '500px' }}>
+        <Title hL={3}>Title Variations</Title>
+        <br></br>div denemesi
+        <br />
+        asdasddasas
+      </BoxMorph>
+      <Grid col={3} row={6} w={'100%'} key={4}>
+        <ButtonSq><i className='fas fa-share-alt'></i></ButtonSq>
+        <Loading w={'50px'} h={'50px'} />
+        <RegularCheckboxIOS>oldumu</RegularCheckboxIOS>
+        <RegularCheckboxIOS checked={true}>oldumu</RegularCheckboxIOS>
+        <Button href={'hamiberkayaktas.tk'}>This is Button</Button>
+        <ButtonPressed>Pressed Button</ButtonPressed>
+
+        <ButtonIL icon='fas fa-user-plus'>Bu bir Soldan iconlu Buton</ButtonIL>
+        <ButtonIR icon='fas fa-user-plus'>Bu bir Sağdan iconlu Buton</ButtonIR>
+        <ButtonwIcon icon='fas fa-arrow-right' />
+        <ButtonwIcon icon='fas fa-arrow-left' />
+
+        <ButtonSmallL icon='fas fa-share-alt'> Share</ButtonSmallL>
+        <ButtonSmallR icon='fas fa-map-marker-alt'> Label</ButtonSmallR>
+        <Input placeholder='merhaba' />
+        <ButtonIconRounded icon='fas fa-search' />
+        {/* <Checkbox >Açıklama</Checkbox>
+      <Checkbox checked={true}>Açıklama</Checkbox>  */}
+        <Select name='cars' id='cars'>
+          <option value='volvo'>Volvo</option>
+          <option value='saab'>Saab</option>
+          <option value='mercedes'>Mercedes</option>
+          <option value='audi'>Audi</option>
+        </Select>
+        <Radio>deneme</Radio>
+        <Radio checked={true}>deneme</Radio>
+
+        <Chart
+          degerler={[50, 20, 75, 30, 85, 10, 99, 5]}
+          w={'200px'}
+          h={'200px'}
+        />
+        <Progress value={38} w={'250px'} h={'10px'} />
+        <CircleProgress value={77} w={'200px'} h={'200px'} />
+        <RegularCheckbox>One</RegularCheckbox>
+        <RegularCheckbox checked={true}>Deneme</RegularCheckbox>
+      </Grid>
       <ExampleComponent text='Create React Library Example 😄' />
+
       <ButtonSq>AI</ButtonSq>
+      <Loading w={'50px'} h={'50px'} />
       <RegularCheckboxIOS>oldumu</RegularCheckboxIOS>
-      <RegularCheckboxIOS checked={true}>oldumu</RegularCheckboxIOS><br/>
-      <Button href={"hamiberkayaktas.tk"}>Bu bir buton</Button>
+      <RegularCheckboxIOS checked={true}>oldumu</RegularCheckboxIOS>
+      <Button href={'hamiberkayaktas.tk'}>This is Button</Button>
       <ButtonPressed>Pressed Button</ButtonPressed>
-      <br />
+
       <ButtonIL icon='fas fa-user-plus'>Bu bir Soldan iconlu Buton</ButtonIL>
-      <ButtonIR icon='fas fa-user-plus'>Bu bir Sağdan iconlu Buton</ButtonIR><br/>
-      <ButtonwIcon icon='fas fa-arrow-right'/>
-      <ButtonwIcon icon='fas fa-arrow-left'/>
-      
+      <ButtonIR icon='fas fa-user-plus'>Bu bir Sağdan iconlu Buton</ButtonIR>
+      <ButtonwIcon icon='fas fa-arrow-right' />
+      <ButtonwIcon icon='fas fa-arrow-left' />
+
       <ButtonSmallL icon='fas fa-share-alt'> Share</ButtonSmallL>
-      <ButtonSmallR icon='fas fa-map-marker-alt'> Label</ButtonSmallR><br/>
-      <Input placeholder='merhaba' /><ButtonIconRounded icon='fas fa-search'/><br/>
-      {/* <Checkbox >Açıklama</Checkbox><br/>
-      <Checkbox checked={true}>Açıklama</Checkbox> <br/> */}
+      <ButtonSmallR icon='fas fa-map-marker-alt'> Label</ButtonSmallR>
+      <Input placeholder='merhaba' />
+      <ButtonIconRounded icon='fas fa-search' />
+      {/* <Checkbox >Açıklama</Checkbox>
+      <Checkbox checked={true}>Açıklama</Checkbox>  */}
       <Select name='cars' id='cars'>
         <option value='volvo'>Volvo</option>
         <option value='saab'>Saab</option>
         <option value='mercedes'>Mercedes</option>
         <option value='audi'>Audi</option>
-      </Select><br/>
-      <Radio>deneme</Radio><br/>
-      <Radio checked={true}>deneme</Radio><br/>
-      
-      <Chart degerler={[50,20,75,30,85,10,99,5]} w={'200px'} h={'200px'}/><br/>
-      <Progress value={38}  w={'250px'} h={'10px'} />
+      </Select>
+      <Radio>deneme</Radio>
+      <Radio checked={true}>deneme</Radio>
+
+      <Chart
+        degerler={[50, 20, 75, 30, 85, 10, 99, 5]}
+        w={'200px'}
+        h={'200px'}
+      />
+      <Progress value={38} w={'250px'} h={'10px'} />
       <CircleProgress value={77} w={'200px'} h={'200px'} />
       <RegularCheckbox>One</RegularCheckbox>
       <RegularCheckbox checked={true}>Deneme</RegularCheckbox>
-      
+      <Dropdown>{[{link:'www.twitter.com',content:'Twitter'},{link:'www.facebook.com',content:'Facebook'},{link:'www.youtube.com',content:'Youtube'}]}</Dropdown>
+
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,69 +5,130 @@ export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
 
-export const Button = ({ children,...rest}) => {
+export const Button = ({ children, ...rest }) => {
   console.log(rest)
-  return <a { ...rest } className={`${styles.morph} ${styles.btn}`}>{children}</a>
-}
-
-export const ButtonSq = ({ children }) => {
   return (
-    <div className={`${styles.btn__square} ${styles.morph} `}>{children}</div>
-  )
-}
-export const ButtonPressed = ({ children }) => {
-  return (
-    <a className={`${styles.btn} ${styles.morph} ${styles.active}`}>
+    <a {...rest} className={`${styles.morph} ${styles.btn}`}>
       {children}
     </a>
   )
 }
-export const ButtonIL = ({ children, icon }) => {
+export const ButtonGroup = ({ children, ...rest }) => {
   return (
-    <a className={`${styles.morph} ${styles.btn} `}>
+    <div className={`${styles.ButtonGroup}`} {...rest}>
+      {children}
+    </div>
+  )
+}
+export const ButtonG = ({ children, ...rest }) => {
+  console.log(rest)
+  return (
+    <a {...rest} className={`${styles.ButtonGroupItem}`}>
+      {children}
+    </a>
+  )
+}
+export const ButtonLG = ({ children, ...rest }) => {
+  console.log(rest)
+  return (
+    <a {...rest} className={`${styles.morph} ${styles.btn} ${styles.btn_lg}`}>
+      {children}
+    </a>
+  )
+}
+export const ButtonSM = ({ children, ...rest }) => {
+  console.log(rest)
+  return (
+    <a {...rest} className={`${styles.morph} ${styles.btn} ${styles.btn_sm}`}>
+      {children}
+    </a>
+  )
+}
+export const ButtonSq = ({ children, ...rest }) => {
+  return (
+    <div className={`${styles.btn__square} ${styles.morph} `} {...rest}>
+      {children}
+    </div>
+  )
+}
+export const ButtonPressed = ({ children, ...rest }) => {
+  return (
+    <a className={`${styles.btn} ${styles.morph} ${styles.active}`} {...rest}>
+      {children}
+    </a>
+  )
+}
+export const ButtonIL = ({ children, icon, ...rest }) => {
+  return (
+    <a className={`${styles.morph} ${styles.btn} `} {...rest}>
       <i className={icon} /> {children}
     </a>
   )
 }
-export const ButtonIR = ({ children, icon }) => {
+export const ButtonIR = ({ children, icon, ...rest }) => {
   return (
-    <a className={`${styles.morph} ${styles.btn} `}>
+    <a className={`${styles.morph} ${styles.btn} `} {...rest}>
       {children} <i className={icon} />
     </a>
   )
 }
-export const ButtonwIcon = ({ icon }) => {
+export const ButtonwIcon = ({ icon, ...rest }) => {
   return (
-    <a className={`${styles.btn__icon} ${styles.morph} `}>
+    <a className={`${styles.btn__icon} ${styles.morph} `} {...rest}>
       <i className={icon} />
     </a>
   )
 }
-export const ButtonIconRounded = ({ icon }) => {
+export const ButtonIconRounded = ({ icon, ...rest }) => {
   return (
     <a
       className={`${styles.btn__icon} ${styles.btn__icon__rounded} ${styles.morph} `}
+      {...rest}
     >
       <i className={icon} />
     </a>
   )
 }
-export const ButtonSmallR = ({ children, icon }) => {
+export const ButtonSmallR = ({ children, icon, ...rest }) => {
   return (
-    <div className={`${styles.btn} ${styles.btn__sm} ${styles.morph}`}>
+    <div
+      className={`${styles.btn} ${styles.btn__sm} ${styles.morph}`}
+      {...rest}
+    >
       {children} <i className={icon}> </i>
     </div>
   )
 }
-export const ButtonSmallL = ({ children, icon }) => {
+export const ButtonSmallL = ({ children, icon, ...rest }) => {
   return (
-    <div className={`${styles.btn} ${styles.btn__sm} ${styles.morph}`}>
+    <div
+      className={`${styles.btn} ${styles.btn__sm} ${styles.morph}`}
+      {...rest}
+    >
       <i className={icon} /> {children}
     </div>
   )
 }
-export const Select = ({ children }) => {
-  return <select className={`${styles.morph} ${styles.btn}`}>{children}</select>
+export const Select = ({ children, ...rest }) => {
+  return (
+    <select
+      className={`${styles.morph} ${styles.btn} ${styles.select_box}`}
+      {...rest}
+    >
+      {children}
+    </select>
+  )
+}
+export const Textarea = ({ children, ...rest }) => {
+  return (
+    <textarea
+      type='text'
+      {...rest}
+      className={`${styles.morph} ${styles.active} ${styles.textareaHolder}`}
+    >
+      {children}
+    </textarea>
+  )
 }
 export const Input = ({ ...rest }) => {
   return (
@@ -95,17 +156,17 @@ export const Checkbox = ({ children, key, ...rest }) => {
   )
 }
 
-export const Radio = ({ children, checked }) => {
+export const Radio = ({ children, checked, ...rest }) => {
   checked = checked || false
   let icon
   let isActive
   console.log(checked)
   if (checked) {
-     icon = 'far fa-dot-circle'
-     isActive = styles.active
+    icon = 'far fa-dot-circle'
+    isActive = styles.active
   } else {
-     icon = 'far fa-circle'
-     isActive = null
+    icon = 'far fa-circle'
+    isActive = null
   }
   return (
     <span>
@@ -113,17 +174,38 @@ export const Radio = ({ children, checked }) => {
         className={`${styles.btn__icon} ${styles.radio} ${styles.morph} ${isActive}`}
       >
         <i className={icon}> </i>
-        <input type='radio' name='deneme' style={{ display: 'none' }} />
+        <input
+          type='radio'
+          name='deneme'
+          style={{ display: 'none' }}
+          {...rest}
+        />
       </div>
       {children}
     </span>
   )
 }
-
-export const Chart = ({ degerler, h, w }) => {
+export const Dropdown = ({ children, ...rest }) => {
+  console.log(children)
+  return (
+    <div className={`${styles.dropdown}`}>
+      <button className={`${styles.dropbtn}`}>Dropdown <i className='fa-solid fa-angle-down'></i></button>
+      <div className={`${styles.dropdownContent}`}>
+        {children.map((item) => (
+          <a href={`${item.link}`}>{item.content}</a>
+        ))}
+      </div>
+    </div>
+  )
+}
+export const Chart = ({ degerler, h, w, ...rest }) => {
   console.log(degerler)
   return (
-    <div className={`${styles.chart}`} style={{ height: h, width: w }}>
+    <div
+      className={`${styles.chart}`}
+      style={{ height: h, width: w }}
+      {...rest}
+    >
       <ul className={`${styles.chart__grid}`}>
         {degerler.map((deger) => (
           <li
@@ -139,12 +221,17 @@ export const Chart = ({ degerler, h, w }) => {
   )
 }
 
-export const Progress = ({ value, h, w  }) => {
+export const Progress = ({ value, h, w, ...rest }) => {
   console.log(value)
   return (
-    <div className={`${styles.progress}`} style={{
-      height: h,
-      width: w}}>
+    <div
+      className={`${styles.progress}`}
+      style={{
+        height: h,
+        width: w
+      }}
+      {...rest}
+    >
       <div
         className={`${styles.progress__item} ${styles.morph} ${styles.active}`}
       >
@@ -157,13 +244,14 @@ export const Progress = ({ value, h, w  }) => {
   )
 }
 
-export const CircleProgress = ({ value, h, w }) => {
+export const CircleProgress = ({ value, h, w, ...rest }) => {
   console.log(value)
   const angle = value * 3.6
   console.log(angle)
   return (
     <div
-      className={`${styles.morph} ${styles.radial_progress}`}
+      className={`${styles.radial_progress}`}
+      {...rest}
       style={{
         height: h,
         width: w,
@@ -177,25 +265,142 @@ export const CircleProgress = ({ value, h, w }) => {
   )
 }
 
-export const RegularCheckbox = ({ children, checked }) => {
+export const RegularCheckbox = ({ children, checked, ...rest }) => {
   return (
     <label className={`${styles.container}`}>
       {children}
-      <input type='checkbox' checked={checked} />
+      <input type='checkbox' checked={checked} {...rest} />
       <span className={`${styles.checkmark}`}></span>
     </label>
   )
 }
-
-export const RegularCheckboxIOS = ({ children, checked,h,w,...rest }) => {
+export const Text = ({ children, ...rest }) => {
+  return <p {...rest}>{children}</p>
+}
+export const TextMuted = ({ children, ...rest }) => {
+  return (
+    <p className={`${styles.muted}`} {...rest}>
+      {children}
+    </p>
+  )
+}
+export const RegularCheckboxIOS = ({ children, checked, h, w, ...rest }) => {
   return (
     <label className={`${styles.containerIO}`}>
-      <span style={{
-        height: h,
-        width: w,marginLeft:'30px'}}>{children}</span>
+      <span
+        style={{
+          height: h,
+          width: w,
+          marginLeft: '30px'
+        }}
+      >
+        {children}
+      </span>
       <input type='checkbox' {...rest} checked={checked} />
       <span className={`${styles.checkmarkIO}`}></span>
     </label>
   )
 }
 
+export const Loading = ({ h, w, ...rest }) => {
+  return (
+    <div
+      className={styles.loader_outer}
+      style={{
+        height: h,
+        width: w
+      }}
+      {...rest}
+    >
+      <div className={styles.loader_border}>
+        <div className={styles.loader_inner}>
+          <div className={styles.loader_inner_shabow}></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export const Grid = ({ col, rows, colGap, rowGap, w, children, ...rest }) => {
+  const colT = col || 1
+  const rowsT = rows || 1
+  const colGapT = colGap || '0px'
+  const rowGapT = rowGap || '0px'
+  const widthT = w || '100%'
+  const gridTemp = {
+    display: 'grid',
+    gridTemplateColumns: `repeat(${colT}, 1fr)`,
+    gridTemplateRows: `repeat(${rowsT}, 1fr)`,
+    gridColumnGap: colGapT,
+    gridRowGap: rowGapT,
+    with: widthT
+  }
+  return (
+    <div style={gridTemp} {...rest}>
+      {children}
+    </div>
+  )
+}
+export const BoxMorph = ({ children, ...rest }) => {
+  return (
+    <div className={`${styles.Box}`} {...rest}>
+      {children}
+    </div>
+  )
+}
+
+export const Box = ({ children, ...rest }) => {
+  return <div {...rest}>{children}</div>
+}
+
+export const Title = ({ hL, children, ...rest }) => {
+  console.log(hL)
+  if (hL == 1) {
+    return (
+      <React.Fragment>
+        <h1 className={`${styles.title}`} {...rest}>
+          {children}
+        </h1>
+      </React.Fragment>
+    )
+  } else if (hL == 2) {
+    return (
+      <React.Fragment>
+        <h2 className={`${styles.title}`} {...rest}>
+          {children}
+        </h2>
+      </React.Fragment>
+    )
+  } else if (hL == 3) {
+    return (
+      <React.Fragment>
+        <h3 className={`${styles.title}`} {...rest}>
+          {children}
+        </h3>
+      </React.Fragment>
+    )
+  } else if (hL == 4) {
+    return (
+      <React.Fragment>
+        <h4 className={`${styles.title}`} {...rest}>
+          {children}
+        </h4>
+      </React.Fragment>
+    )
+  } else if (hL == 5) {
+    return (
+      <React.Fragment>
+        <h5 className={`${styles.title}`} {...rest}>
+          {children}
+        </h5>
+      </React.Fragment>
+    )
+  } else if (hL == 6) {
+    return (
+      <React.Fragment>
+        <h6 className={`${styles.title}`} {...rest}>
+          {children}
+        </h6>
+      </React.Fragment>
+    )
+  }
+}
